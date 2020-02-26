2d1893cd2959a632da850c3d3cc13f118ec20c45
Jeremy Hansen <jhansen1344@gmail.com>
2020-02-26 14:57:54 -0500

    Add project files.

---
+++ GeneralStoreAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        //This is how we will interact with the customer table.  since we have sql to c#, we need a property
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
---
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Transaction
+    { 
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public virtual Customer Customer { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+        public double TotalCost => Product.Price * ProductCount;
+        public int ProductCount { get; set; }
+    }
+}
